aaa03c79753c715a6d331caecd82bf6e35cc2a0d
mgk5 <mohamed.kurani@outlook.com>
2017-03-22 09:37:21 -0700

    Receiver Code March 22

---
+++ Receiver_Code_March_22.ino
@@ -0,0 +1,38 @@
+#include <Manchester.h>
+
+
+int RX_PIN = 2;     // Receiver connected to digital pin 2
+
+int val = 0;           // variable to store the value read 
+
+char message[]="";
+
+void setup()
+
+{
+  //Setup for receiving data
+  man.setupReceive(RX_PIN, MAN_1200);
+  man.beginReceive();
+  Serial.begin(1200);          //  setup serial
+  pinMode(RX_PIN, INPUT); 
+
+}
+
+
+void loop()
+
+{ 
+
+  // Loop for receiving data 
+
+  if (man.receiveComplete()) {
+    uint16_t m = man.getMessage();
+    man.beginReceive(); 
+
+    
+   Serial.println(m); 
+    
+    //start listening for next message right after you retrieve the message
+    //do something with your message here
+  }
+}
